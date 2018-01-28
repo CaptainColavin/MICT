9bcce7bdd5b50d0c6ff69d7d5b99d1481e1e4e25
CaptainColavin <soleymani.colin@gmail.com>
2018-01-28 18:47:46 +0100

    Add empty pages with routing

--- src/frontBundle/Controller/DefaultController.php
+++ src/frontBundle/Controller/DefaultController.php
@@ -14,4 +14,44 @@ class DefaultController extends Controller
     {
         return $this->render('frontBundle:Default:index.html.twig');
     }
+
+    /**
+     * @Route("/offres")
+     */
+    public function offre()
+    {
+        return $this->render('frontBundle:Default:offres.html.twig');
+    }
+
+    /**
+     * @Route("/photos")
+     */
+    public function photos()
+    {
+        return $this->render('frontBundle:Default:photos.html.twig');
+    }
+
+    /**
+     * @Route("/livredor")
+     */
+    public function book()
+    {
+        return $this->render('frontBundle:Default:goldenbook.html.twig');
+    }
+
+    /**
+     * @Route("/contact")
+     */
+    public function contact()
+    {
+        return $this->render('frontBundle:Default:contact.html.twig');
+    }
+
+    /**
+     * @Route("/boutique")
+     */
+    public function shop()
+    {
+        return $this->render('frontBundle:Default:boutique.html.twig');
+    }
 }
