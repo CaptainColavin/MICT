491bcdfaf58033e805f5796cd3d19c6715041d81
Anthony Barei <anthony.barei@outlook.fr>
2018-02-08 09:48:49 +0100

    backoffice pages

--- src/BackBundle/Controller/DefaultController.php
+++ src/BackBundle/Controller/DefaultController.php
@@ -8,10 +8,51 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/back")
+     * @Route("/gestion")
      */
     public function indexAction()
     {
         return $this->render('BackBundle:Default:index.html.twig');
+    }
+
+
+    /**
+     * @Route("/gestion/offre")
+     */
+    public function offreAction()
+    {
+        return $this->render('BackBundle:Default:offre.html.twig');
+    }
+
+    /**
+     * @Route("/gestion/photo")
+     */
+    public function photoAction()
+    {
+        return $this->render('BackBundle:Default:photo.html.twig');
+    }
+
+    /**
+     * @Route("/gestion/livre")
+     */
+    public function livreAction()
+    {
+        return $this->render('BackBundle:Default:livre.html.twig');
+    }
+
+    /**
+     * @Route("/gestion/contact")
+     */
+    public function contactAction()
+    {
+        return $this->render('BackBundle:Default:contact.html.twig');
+    }
+
+    /**
+     * @Route("/gestion/boutique")
+     */
+    public function boutiqueAction()
+    {
+        return $this->render('BackBundle:Default:boutique.html.twig');
     }
 }
